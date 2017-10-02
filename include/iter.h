dbcd0f02a7b0d47f4a59b05d04e734c4093c04cc
Adorjan White <addowhite@users.noreply.github.com>
2017-10-02 23:23:15 +0100

    Added 'range' macro + hashmap iter tests
    - Added 'range' macro to iterate over a range of integers
    - Added tests for iterating over a hashmap

--- include/iter.h
+++ include/iter.h
@@ -38,6 +38,9 @@
                                             for (uchar **FOREACH_PTR_##iter = iterable->data, **FOREACH_END_##iter = iterable->data + iterable->size; FOREACH_PTR_##iter != FOREACH_END_##iter; ++FOREACH_PTR_##iter)\
                                                 for (type* iter = (type*)(*FOREACH_PTR_##iter), *FOREACH_FLAG2_##iter = NULL; FOREACH_FLAG2_##iter == NULL; FOREACH_FLAG2_##iter = (void *)1)
 //*/
+
+#define range(index, start, end) (int index = start, END_##index = range_end(index, end); index != END_##index; index = range_next(index, END_##index))
+
 typedef enum IterType {
     ITER_TYPE_VECTOR,
     ITER_TYPE_GRID,
@@ -50,6 +53,8 @@ typedef struct Iterable {
     uint size;
 } Iterable;
 
+static int range_next(int index, int end) { return (index < end) ? ++index : --index; };
+static int range_end(int start, int end) { return (start < end) ? ++end : --start; };
 Vector * linearize_iterable(Iterable *iterable);
 
 #endif // _ITER_H_
--- test/test_iter.c
+++ test/test_iter.c
@@ -1,12 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include "test_iter.h"
 #include "iter.h"
 #include "vector.h"
 #include "grid.h"
+#include "hashmap.h"
 
-static uint test_iter_vector() {
+static char * string_fill_random(char *str, uint count) {
+  char base = 'A';
+  for (uint i = 0; i < count; ++i)
+    str[i] = base + rand() % 26;
+  return str;
+}
+
+static uint test_iter_vector(void) {
 	Vector *vector = vector_create();
 	if (!vector)
     return test_failed("test_iter_vector", "Failed to create vector for test", __FILE__, __LINE__);
@@ -30,7 +39,7 @@ static uint test_iter_vector() {
 	return SUCCESS;
 }
 
-static uint test_iter_grid() {
+static uint test_iter_grid(void) {
 	Grid *grid = grid_create(100, 100);
 	if (!grid)
     return test_failed("test_iter_grid", "Failed to create grid for test", __FILE__, __LINE__);
@@ -55,6 +64,88 @@ static uint test_iter_grid() {
 	return SUCCESS;
 }
 
+static uint test_iter_hashmap(void) {
+  HashMap *hashmap = hashmap_create();
+
+  uint value_count = 20;
+  char keys[20][11];
+  int values[20];
+  for (uint i = 0; i < value_count; ++i) {
+    values[i] = rand() % 0x11111111;
+    string_fill_random(keys[i], 10);
+    keys[i][10] = 0;
+    hashmap_set(hashmap, keys[i], &values[i]);
+  }
+
+  foreach(HashMapValueContainer, hashmap, iter)
+    if (hashmap_get(hashmap, iter->key) != iter->value)
+      return test_failed("test_iter_hashmap", "Key-value pair incorrect", __FILE__, __LINE__);
+
+  hashmap_destroy(hashmap);
+  return SUCCESS;
+}
+
+static uint test_iter_range(void) {
+  // Test iterating forward
+  {
+    uint test_index = 0;
+    for range(i, 0, 10)
+      if (i != test_index++)
+        return test_failed("test_iter_range", "Index was incorrect when iterating range", __FILE__, __LINE__);
+
+    int test_x = -5, test_y = -5;
+    for range(y, -5, 5) {
+      for range(x, -5, 5) {
+        if (x != test_x)
+          return test_failed("test_iter_range", "Index was incorrect when iterating range", __FILE__, __LINE__);
+
+        if (y != test_y)
+          return test_failed("test_iter_range", "Index was incorrect when iterating range", __FILE__, __LINE__);
+
+        if (++test_x > 5) { test_x = -5; test_y++; }
+      }
+    }
+  }
+
+  // Test iterating backward
+  {
+    uint test_index = 10;
+    for range(i, 10, 0)
+      if (i != test_index--)
+        return test_failed("test_iter_range", "Index was incorrect when iterating range", __FILE__, __LINE__);
+
+    int test_x = 5, test_y = 5;
+    for range(y, 5, -5) {
+      for range(x, 5, -5) {
+        if (x != test_x)
+          return test_failed("test_iter_range", "Index was incorrect when iterating range", __FILE__, __LINE__);
+
+        if (y != test_y)
+          return test_failed("test_iter_range", "Index was incorrect when iterating range", __FILE__, __LINE__);
+
+        if (--test_x < -5) { test_x = 5; test_y--; }
+      }
+    }
+  }
+
+  // Test each argument is evaluated exactly once
+  {
+    int lower = 0, upper = 0, loop_count = 0;
+    for range(i, ++lower, ++upper + 4) loop_count++;
+
+    if (lower != 1)
+      return test_failed("test_iter_range", "Range lower bound is not evaluated exactly once", __FILE__, __LINE__);
+
+    if (upper != 1)
+      return test_failed("test_iter_range", "Range upper bound is not evaluated exactly once", __FILE__, __LINE__);
+
+    if (loop_count != 5)
+      return test_failed("test_iter_range", "Incorrect number of iterations in range", __FILE__, __LINE__);
+  }
+
+  return SUCCESS;
+}
+
 uint test_iter(void) {
 	printf("\nTesting iter...\n");
 
@@ -64,6 +155,8 @@ uint test_iter(void) {
 
 	CHECK_TEST(test_iter_vector());
 	CHECK_TEST(test_iter_grid());
+  CHECK_TEST(test_iter_hashmap());
+  CHECK_TEST(test_iter_range());
 
 	printf("All iter tests passed!\n");
 }
